e14e1bf30783dbe37028c1d84edf5023cb0f889c
guoxiaoshangit <564499370@qq.com>
2017-03-11 14:33:22 +0800

    Update Hello.java File Add Two Lines

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello
 {
 	public static void main(String args){
 		System.out.println("Hello GIT");
+		System.out.println("Hello GIT modify1");
+		System.out.println("Hello GIT modify2");
 	}
 }
