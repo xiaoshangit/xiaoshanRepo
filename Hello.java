584b4758d1ad1caa3bd9d20fb085191c0cd6964f
guoxiaoshangit <564499370@qq.com>
2017-03-11 18:34:40 +0800

    sdfsdfsdfsdf

--- Hello.java
+++ Hello.java
@@ -2,8 +2,6 @@ public class Hello
 {
 	public static void main(String args){
 		System.out.println("Hello GIT");
-		System.out.println("Hello GIT modify1");
-		System.out.println("Hello GIT modify2");
-		System.out.println("Hello GIT first  modify");
+		
 	}
 }
