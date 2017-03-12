4487bcb6bb90d6db7de36da430e771e45bc5c04a
guoxiaoshangit <564499370@qq.com>
2017-03-12 14:33:30 +0800

    Patch Apple

--- Emp.java
+++ Emp.java
@@ -1,6 +1,11 @@
 import java.io.*
+import java.util.*
 public class Emp implements Serializable
 {
 	private Integer empno;
 	private String ename;
+	private Date hiredate;
+	private Double sal;
+	private Double comm;
+	private Emp mgr;
 }
